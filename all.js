d3e43a116b46346d26677bd89a47951c5515abc0
xym5366 <xym5366@qq.com>
2018-02-13 16:45:05 +0800

    Update all.js

--- all.js
+++ all.js
@@ -24,11 +24,11 @@ window.addEventListener("load", function () {
       alert('Oups! 出错。');
     });
     // 组建tbs GET请求
-    tbs_request.open("GET", "http://tieba.baidu.com/dc/common/tbs");
+    tbs_request.open("GET", "http://tieba.baidu.com/dc/common/tbs",true);
     tbs_request.setRequestHeader('Content-Type','application/x-www-form-urlencoded; charset=UTF-8');
     tbs_request.send();
     // 组建POST请求
-    request_mes.open("POST", "http://tieba.baidu.com/pmc/tousu/commitTousu");
+    request_mes.open("POST", "http://tieba.baidu.com/pmc/tousu/commitTousu",true);
     request_mes.setRequestHeader('Content-Type','application/x-www-form-urlencoded; charset=UTF-8');
     // 发送的数据，由用户输入提供一些
     data='manager_uid='+manager_uid.value+'&'+'manager_uname='+manager_uname.value+'&forum_id=22545&forum_name=c%E8%AF%AD%E8%A8%80&complaint_type=illegal_delpost&reason=%E4%B9%B1%E5%88%A0%E8%B4%B4&tbs='+tbs
